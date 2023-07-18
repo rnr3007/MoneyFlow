c8bd604b35c23c099eb4b04f327908ec96b8e763
rnr3007 <rivarahim@gmail.com>
2023-07-18 16:01:02 +0700

    Feat: Input validator

--- Services/UserService.cs
+++ Services/UserService.cs
@@ -5,7 +5,7 @@ using MoneyFlow.Constants;
 using MoneyFlow.Context;
 using MoneyFlow.Models;
 using MoneyFlow.Utils;
-using iv = mvc.Utils.InputValidator;
+using iv = MoneyFlow.Utils.InputValidator;
 
 namespace MoneyFlow.Services
 {
@@ -20,33 +20,23 @@ namespace MoneyFlow.Services
 
         public string Register(User user)
         {
-            if (!iv.IsUsernameValid(user.Username)){ throw new FormatException(ErrorMessage.USERNAME_FORMAT_INVALID); }
-            if (!iv.IsPasswordValid(user.Password)){ throw new FormatException(ErrorMessage.PASSWORD_FORMAT_INVALID); }
-            if (!iv.IsFullnameValid(user.FullName)){ throw new FormatException(ErrorMessage.FULLNAME_FORMAT_INVALID); }
-            if (!iv.IsEmailValid(user.Email)){ throw new FormatException(ErrorMessage.EMAIL_FORMAT_INVALID); }
-
             user.Password = BCrypt.Net.BCrypt.HashPassword(user.Password);
             _context.Users.Add(user);
             var jwtToken = AuthUtilites.GenerateJwt(user);
             _context.SaveChanges();
-            return jwtToken;
         }
 
         public string Login(User user)
         {
-            if (!iv.IsNotEmpty(user.Username) || !iv.IsNotEmpty(user.Password)) { throw new FormatException(ErrorMessage.FIELD_EMPTY); }
             var userResult = _context.Users.Where(x => x.Username == user.Username).FirstOrDefault() ?? throw new DataException(ErrorMessage.USER_NOT_FOUND);
             if (!BCrypt.Net.BCrypt.Verify(user.Password, userResult.Password)) { throw new DataException(ErrorMessage.WRONG_PASSWORD);}
             var jwtToken = AuthUtilites.GenerateJwt(userResult);
+            
             return jwtToken;
         }
 
         public string Edit(User user)
         {
-            if (!iv.IsUsernameValid(user.Username)){ throw new FormatException(ErrorMessage.USERNAME_FORMAT_INVALID); }
-            if (!iv.IsPasswordValid(user.Password)){ throw new FormatException(ErrorMessage.PASSWORD_FORMAT_INVALID); }
-            if (!iv.IsFullnameValid(user.FullName)){ throw new FormatException(ErrorMessage.FULLNAME_FORMAT_INVALID); }
-            if (!iv.IsEmailValid(user.Email)){ throw new FormatException(ErrorMessage.EMAIL_FORMAT_INVALID); }
             user.Password = BCrypt.Net.BCrypt.HashPassword(user.Password);
             _context.Update(user);
             var jwtToken = AuthUtilites.GenerateJwt(user);
